129e0e3f5a43c3a9d31f7168b71277004cddaba3
unknown <quentin.joubert11800@gmail.com>
2024-11-04 20:01:35 +0100

    modif fichier ex1 python

--- PYTHON/Ex1.py
+++ PYTHON/Ex1.py
@@ -1,3 +1,4 @@
+# Afficher le menu contenant les options
 def afficher_menu():
     print("\n") 
     print("1 : Ajouter une tâche")
@@ -5,6 +6,7 @@ def afficher_menu():
     print("3 : Supprimer une tâche")
     print("4 : Quitter")
 
+# Choisir une des options de la fonction "afficher_menu()"
 def choisir_option():
     taches = []
 
@@ -12,12 +14,12 @@ def choisir_option():
         afficher_menu()
         print("\n")
         choix = input("Choisir une option avec le numéro : ")
-
+# Option 1
         if choix == '1':
             tache = input("Nom de la tâche : ")
             taches.append(tache)
             print(f"Tâche ajoutée : {tache}")
-        
+   # Option 2     
         elif choix == '2':
             if taches:
                 print("Liste des tâches :")
@@ -25,7 +27,7 @@ def choisir_option():
                     print(f"{i} : {tache}")
             else:
                 print("Aucune tâche à afficher.")
-        
+    # Option 3
         elif choix == '3':
             if taches:
                 afficher_taches(taches)
@@ -41,18 +43,18 @@ def choisir_option():
                     print("Un numéro valide doit être rentré.")
             else:
                 print("Aucune tâche à supprimer.")
-        
+    # Option 4
         elif choix == '4':
             break
         
         else:
             print("Choix invalide.")
-
+# Afficher la liste des tâches
 def afficher_taches(taches):
     print("Liste des tâches :")
     for i, tache in enumerate(taches, start=1):
         print(f"{i} : {tache}")
-
+# Afficher la fonction "choisir_option" si le nom est égal à "main"
 if __name__ == "__main__":
     choisir_option()
 
